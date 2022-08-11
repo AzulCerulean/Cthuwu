1437c856b0a7eac98809b969527106956fa2d769
Azul Cerulean <azul.sama@outlook.com>
2022-08-11 13:55:20 -0400

    header

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
+import Header from "./Header";
+
 const App = () => {
   return (
-    <h1>MY React App is live</h1>
+    <div>
+      <h1>MY React App is live</h1>
+      {/* header */}
+      <Header />
+      {/* recipe cards */}
+      {/* buttons */}
+    </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div><h2>I am a header</h2></div>
+  )
+};
+
+export default Header;
